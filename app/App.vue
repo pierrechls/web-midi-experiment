2ed821919ef6c7922bd948c682b553a435e64bea
Pierre Charles <pierre.chls@gmail.com>
2017-01-08 17:18:27 +0100

    Update App.vue file

---
+++ app/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>{{ msg }}</h1>
+    <links></links>
+  </div>
+</template>
+
+<script>
+
+  import Links from 'components/Links'
+
+  export default {
+    name: 'App',
+    components: { Links },
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
+
+</script>
+
+<style>
+
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  h1, h2 {
+    font-weight: normal;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
+
+</style>
