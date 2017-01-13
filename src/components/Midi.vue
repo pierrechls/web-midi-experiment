7ca0ba7f2da638821acf0d513f239f23b37179a4
Pierre Charles <pierre.chls@gmail.com>
2017-01-13 08:53:11 +0100

    Add Midi component

---
+++ src/components/Midi.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>Hello</h1>
+</template>
+
+<script>
+
+  import WebMidi from 'webmidi'
+
+  export default {
+    name: 'Midi',
+    data () {
+      return {}
+    },
+    methods: {},
+    mounted () {}
+  }
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
