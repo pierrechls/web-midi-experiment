f8b9514587fab441663a6873a808d57c48e998a1
Pierre Charles <pierre.chls@gmail.com>
2017-01-18 12:34:50 +0100

    Add MidiController Component

---
+++ src/components/MidiController.vue
@@ -0,0 +1,33 @@
+<template>
+  <h1>Hello</h1>
+</template>
+
+<script>
+
+  import WebMidi from 'webmidi'
+
+  export default {
+    name: 'MidiController',
+    data () {
+      return {}
+    },
+    methods: {},
+    mounted () {
+
+      WebMidi.enable(function (err) {
+        if (err) {
+          console.log('WebMidi could not be enabled.', err)
+        } else {
+          console.log('WebMidi enabled!')
+        }
+      })
+
+    }
+
+  }
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
